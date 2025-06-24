53134cceec5ee29470b21fb207807df79ea84738
pythonicforge <pythonicforge@gmail.com>
2025-06-25 04:08:20 +0530

    feat: logs

--- core/llm_wrapper/transformer_llm.py
+++ core/llm_wrapper/transformer_llm.py
@@ -11,7 +11,6 @@ class TransformerLLM:
         self.local_dir = os.path.join(local_dir, model_id.split("/")[-1])
 
         if not os.path.exists(self.local_dir) or not os.listdir(self.local_dir):
-            # Download from Hugging Face
             logger.info(f"Downloading model: {self.model_id} [{self.local_dir}]")
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_id)
             self.model = AutoModelForCausalLM.from_pretrained(self.model_id)
@@ -20,15 +19,9 @@ class TransformerLLM:
             self.model.save_pretrained(self.local_dir)
             logger.success("Download completed!")
         else:
-            # Load from local directory
+            logger.info("Model found at local directory! Loading..")
             self.tokenizer = AutoTokenizer.from_pretrained(self.local_dir)
             self.model = AutoModelForCausalLM.from_pretrained(self.local_dir)
-
-    def download_model(self):
-        logger.info(f"Downloading model: {self.model_id} [{self.local_dir}]")
-        self.tokenizer.save_pretrained(self.local_dir)
-        self.model.save_pretrained(self.local_dir)
-        logger.success("Download completed!")
 
     def quantize_to_gguf(self, quant_type="f16", output_dir="models/quantized"):
         gguf_name = f"{self.model_id.split('/')[-1]}-{quant_type}.gguf"
@@ -38,14 +31,14 @@ class TransformerLLM:
 
         logger.info("Starting quantization process...")
         cmd = [
-    "python",
-    "llama.cpp/convert_hf_to_gguf.py",
-    self.local_dir,            # positional argument for model path
-    "--outfile",
-    output_path,
-    "--outtype",
-    quant_type
-]
+            "python",
+            "llama.cpp/convert_hf_to_gguf.py",
+            self.local_dir,
+            "--outfile",
+            output_path,
+            "--outtype",
+            quant_type
+        ]
 
         subprocess.run(cmd, check=True)
         logger.success(f"Quantised model saved at: {output_path}")
